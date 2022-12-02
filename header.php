9e3e6a89cccf077cdffdbf81857de4d7f92779a8
mahdi1384sha <118567379+mahdi1384sha@users.noreply.github.com>
2022-12-02 23:08:55 +0330

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Abzar kadeh</title>
+</head>
+<body>
+    <header>
+        <h1>Abzar Kadeh</h1>
+        <div class="logo">
+            <img src="image/logo1.jpg" alt="ABZAR KADEH">
+        </div>
+        <button class="button">Log in</button>
+
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+include 'header.php';
+include 'nav.php'; 
+?>
+<br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="articals">
+        <article>
+    <H1>Abzar Ha</H1>
+        <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   </p>
+</article>
+</div>
+    <div>
+    <div class="polaroid">
+  <img src="image/logo-.jpg" alt="Norway">
+  <div class="container">
+  <button class="container">View products</button>
+
+  </div>
+</div>
+<div class="polaroid1">
+  <img src="image/logo-1.jpg" alt="Norway">
+  <div class="container1">
+  <button class="container1">View products</button>
+  </div>
+</div>
+<div class="polaroid2">
+  <img src="image/logo-2.jpg" alt="Norway">
+  <div class="container2">
+  <button class="container2">View products</button>
+  </div>
+</div>
+<div class="polaroid3">
+  <img src="image/logo-3.jpg" alt="Norway">
+  <div class="container3">
+  <button class="container3">View products</button>
+  </div>
+</div>
+<div class="polaroid4">
+  <img src="image/logo-4.jpg" alt="Norway">
+  <button class="container4">View products</button>
+  
+</div>
+    </div>
+    
+
+    <div class="imd">
+        <h1>bakgerand</h1>
+  <img src="image/logo--.jpg" alt="Cinque Terre">
+  <div class="center">Abzar kadeh</div>
+</div>
+<div class="text">
+<h1>Nmone digar....</h1>
+<br>
+<p>
+So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.
+
+It's difficult to find examples of lorem ipsum in use before Letraset made it popular as a dummy text in the 1960s, although McClintock says he remembers coming across the lorem ipsum passage in a book of old metal type samples. So far he hasn't relocated where he once saw the passage, but the popularity of Cicero in the 15th century supports the theory that the filler text has been used for centuries.
+</p>
+<br>
+<div class="foter">
+<?php
+include 'footer.php';
+?>
+</div>
+</div>
+</body>
+</html>
+<br><br>
+
